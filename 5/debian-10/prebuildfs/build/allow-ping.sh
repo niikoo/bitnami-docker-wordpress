7ea281514c1b5734504e1ccb964af4e3368655b9
Nikolai Ommundsen <post@niikoo.net>
2020-09-03 22:38:37 +0200

    Work on xdebug connection to host

--- 5/debian-10/prebuildfs/build/allow-ping.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chmod -R g+rwX /bin/ping
-chmod -R g+rwX /bin/ping6
-setcap cap_net_admin,cap_net_bind_service,cap_net_raw+eip /bin/ping
-setcap cap_net_admin,cap_net_bind_service,cap_net_raw+eip /bin/ping6
-sysctl -w net.ipv4.ping_group_range='0 2147483647'
-echo "net.ipv4.ping_group_range='0 2147483647'" >> /etc/sysctl.conf
--- 5/debian-10/prebuildfs/build/setup-xdebug.sh
+++ 5/debian-10/prebuildfs/build/setup-xdebug.sh
@@ -2,7 +2,8 @@
 
 echo "Enabling XDebug on port $XDEBUG_PORT_NUMBER"
 export PHP_INI_ACTIVE=/opt/bitnami/php/lib/php.ini
-sed -i -e 's/;xdebug.remote_port = 9000*/xdebug.remote_port = $XDEBUG_PORT_NUMBER/g' $PHP_INI_ACTIVE
-sed -i -e 's/;xdebug.*/xdebug./g' $PHP_INI_ACTIVE
+sed -i -e 's/;xdebug.remote_port = 9000*/xdebug.remote_port = 9001/g' $PHP_INI_ACTIVE
+sed -i -e 's/;xdebug.remote_host = 127.0.0.1*/xdebug.remote_host = host.docker.internal/g' $PHP_INI_ACTIVE
+sed -i -e 's/;xdebug./xdebug./g' $PHP_INI_ACTIVE
 sed -i -e 's/;[XDebug] /[XDebug] /g' $PHP_INI_ACTIVE
 sed -i -e 's/;zend_extension = xdebug*/zend_extension = xdebug/g' $PHP_INI_ACTIVE
